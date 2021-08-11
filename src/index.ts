bbc546da74c49c787c0de267263838fa2853c87d
Erik C. Forés <soc@erik.cat>
2021-08-11 23:49:39 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,7 @@ export class SSWR extends SWR {
   /**
    * Svelte specific use of SWR.
    */
-  useSvelte<D = any, E = Error>(
-    key: SWRKey | undefined | (() => SWRKey | undefined),
-    options?: Partial<SWROptions<D>>
-  ) {
+  useSWR<D = any, E = Error>(key: SWRKey | undefined | (() => SWRKey | undefined), options?: Partial<SWROptions<D>>) {
     let unsubscribe: undefined | (() => void) = undefined
 
     // Contains the data and errors stores.
@@ -60,25 +57,108 @@ export class SSWR extends SWR {
 }
 
 /**
- * Creates a new SWR instance and exports basic methods to
+ * Creates a mew SWR instance and exports basic methods to
  * work with without the need for method calling.
  */
-export const createSWR = <G = unknown>(options?: Partial<SWROptions<G>>) => {
-  const swr = new SSWR(options)
-  return {
-    useSWR: <D = G, E = Error>(key: SWRKey, options?: Partial<SWROptions<D>>) => swr.useSvelte<D, E>(key, options),
-    mutate: <D = G>(key: SWRKey, value: D, options?: Partial<SWRMutateOptions<D>>) =>
-      swr.mutate<D>(key, value, options),
-    revalidate: <D = G>(key: SWRKey, options?: Partial<SWRRevalidateOptions<D>>) => swr.revalidate<D>(key, options),
-    clear: (keys?: string | string[], options?: Partial<CacheClearOptions>) => swr.clear(keys, options),
-  }
+export const createSWR = <D = any>(options?: Partial<SWROptions<D>>) => new SSWR(options)
+
+/**
+ * Default SWR instance. Can be replaced
+ * with the `createDefaultSWR` function.
+ */
+export let swr = createSWR()
+
+/**
+ * Creates and sets a default SWR instance given
+ * the options.
+ */
+export const createDefaultSWR = <D = any>(options?: Partial<SWROptions<D>>) => {
+  swr = createSWR<D>(options)
+  return swr
 }
 
-// Default SWR instance to play with.
-const swr = createSWR()
+/**
+ * Gets the data of the given key. Keep in mind
+ * this data will be stale and revalidate in the background
+ * unless specified otherwise.
+ */
+export const subscribe = <D>(key: SWRKey | undefined, onData: (value: D) => any) => {
+  return swr.subscribe<D>(key, onData)
+}
 
-// Default instance exports.
-export const useSWR = swr.useSWR
-export const mutate = swr.mutate
-export const revalidate = swr.revalidate
-export const clear = swr.clear
+/**
+ * Subscribes to errors on the given key.
+ */
+export const subscribeErrors = <E>(key: SWRKey | undefined, onError: (error: E) => any) => {
+  return swr.subscribeErrors<E>(key, onError)
+}
+
+/**
+ * Gets the current cached data of the given key.
+ * This does not trigger any revalidation nor mutation
+ * of the data.
+ * - If the data has never been validated
+ * (there is no cache) it will return undefined.
+ * - If the item is pending to resolve (there is a request
+ * pending to resolve) it will return undefined.
+ */
+export const get = <D = any>(key?: SWRKey): D | undefined => {
+  return swr.get<D>(key)
+}
+
+/**
+ * Gets an element from the cache. The difference
+ * with the get is that this method returns a promise
+ * that will resolve the the value. If there's no item
+ * in the cache, it will wait for it before resolving.
+ */
+export const getOrWait = <D = any>(key: SWRKey): Promise<D> => {
+  return swr.getOrWait<D>(key)
+}
+
+/**
+ * Use a SWR value given the key and
+ * subscribe to future changes.
+ */
+export const use = <D = any, E = Error>(
+  key: SWRKey | undefined | (() => SWRKey | undefined),
+  onData: (value: D) => void,
+  onError: (error: E) => void,
+  options?: Partial<SWROptions<D>>
+) => {
+  return swr.use<D, E>(key, onData, onError, options)
+}
+
+/**
+ * Use the SWR with a vue application.
+ */
+export const useSWR = <D = any, E = Error>(
+  key: SWRKey | undefined | (() => SWRKey | undefined),
+  options?: Partial<SWROptions<D>>
+) => {
+  return swr.useSWR<D, E>(key, options)
+}
+
+/**
+ * Mutates the data of a given key with a new value.
+ * This is used to replace the cache contents of the
+ * given key manually.
+ */
+export const mutate = <D = any>(key: SWRKey, value: D, options?: Partial<SWRMutateOptions<D>>) => {
+  return swr.mutate<D>(key, value, options)
+}
+
+/**
+ * Revalidates the key and mutates the cache if needed.
+ */
+export const revalidate = <D>(key: SWRKey, options?: Partial<SWRRevalidateOptions<D>>) => {
+  return swr.revalidate(key, options)
+}
+
+/**
+ * Clear the specified keys from the cache. If no keys
+ * are specified, it clears all the cache keys.
+ */
+export const clear = (keys?: string | string[], options?: Partial<CacheClearOptions>) => {
+  return swr.clear(keys, options)
+}
